6f3fdc7ad31e5d685fa459dfc7c999964dfb9eb9
baumannalexj <baumannalexj@gmail.com>
2018-06-06 15:17:08 -0500

    trying to reuse html, but inconsistent or race conditions for running the iifys

---
+++ widgets/thisworks.js
@@ -0,0 +1,25 @@
+// $(function () {
+//     var header =
+//         `
+//     <div class="mdl-layout__header-row portfolio-logo-row">
+//                 <span class="mdl-layout__title">
+//                     <div class="portfolio-logo"></div>
+//                     <span class="mdl-layout__title">Ask Chicago, Now.</span>
+//                 </span>
+//     </div>
+//     <div class="mdl-layout__header-row portfolio-navigation-row mdl-layout--large-screen-only">
+//         <nav class="mdl-navigation mdl-typography--body-1-force-preferred-font">
+//             <a class="mdl-navigation__link" href="index.html">Home</a>
+//             <a class="mdl-navigation__link" href="privacy-policy.html">Privacy Policy</a>
+//             <a class="mdl-navigation__link" href="contact.html">Contact</a>
+//         </nav>
+//     </div>`
+//
+// });
+
+    // $(".mdl-layout__header.mdl-layout__header--waterfall.header")
+    //     .load("./widgets/header.html");
+
+    var x = 'this works';
+
+    console.log(3);
